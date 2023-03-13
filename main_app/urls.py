257de2c8b1ffa6b591f5b664a268e11ecf23002d
Lemmy Onukwugha <lemmyonukwugha@Lemmys-MacBook-Pro.local>
2023-03-13 18:21:15 -0400

    creating CRUD for the application

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('pictures/<int:picture_id>/add_photo/', views.add_photo, name='add_photo')
+    path('pictures/<int:picture_id>/add_photo/', views.add_photo, name='add_photo'),
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
