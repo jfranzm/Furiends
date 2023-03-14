78fbce5474ff345f1147a6629a1175599221886e
abuzarbt2002 <abuzarbt2002@yahoo.com>
2023-03-14 19:38:47 -0400

    created models, and updated views

--- main_app/models.py
+++ main_app/models.py
@@ -1,21 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+TYPE = (
+   (1, 'Profile'),
+   (2, 'Pets')
+)
+
 class Photo(models.Model):
   url = models.CharField(max_length=200)
-  
+  caption = models.CharField(max_length=250)
+  category = models.IntegerField(choices=TYPE)
+  date = models.DateField(default = date.today)
+  likes = models.BigIntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
-    return f"Photo for post_id: {self.post_id} @{self.url}"
+    return f"Photo for post_id: {self.caption} with {self.likes} uploaded on {self.date} by user {self.user}"
   
+
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.TextField(max_length=250)
-    date = models.DateTimeField()
-    photo = models.ForeignKey(Photo, max_length=250)
-    
+    date = models.DateField(default = date.today) 
+    likes = models.BigIntegerField()   
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Photo for post_id: {self.post_id} @{self.url}"
+        return f"Photo for post_id: {self.caption} created by user {self.user} on {self.date}"
     
--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('my_profile/', views.my_profile, name='my_profile'), 
-    path('post/create/', views.PostCreate.as_view(), name='post_create'),
+    path('post/create/', views.PostCreate, name='post_create'),
     path('my_profile/<int:post_id>/add_photo/', views.add_photo, name='add_photo'),
     path('my_profile/<int:post_id>/', views.post_detail, name='post_detail')
 
