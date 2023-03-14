bafd002b4801a225d3bf48c8ae4f26ee0455e099
LemmyOnukwugha <114013405+LemmyOnukwugha@users.noreply.github.com>
2023-03-14 19:45:19 -0400

    pull from new base

--- main_app/models.py
+++ main_app/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  
+
+  def __str__(self):
+    return f"Photo for post_id: {self.post_id} @{self.url}"
+  
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.TextField(max_length=250)
     date = models.DateTimeField()
+    photo = models.ForeignKey(Photo, max_length=250)
+    
 
     def __str__(self):
         return f"Photo for post_id: {self.post_id} @{self.url}"
     
-class Photo(models.Model):
-  url = models.CharField(max_length=200)
-  post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-  def __str__(self):
-    return f"Photo for post_id: {self.post_id} @{self.url}"
